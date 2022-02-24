2ecc4bc9ca9bb171112f70e1f7939691cf2968c0
0xB4D4SS <37245348+0xB4D4SS@users.noreply.github.com>
2022-02-24 12:56:40 +0400

    trying to make server work

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
 import os
 import socket
-
-import server
+import socketserver
+import sys
 from multiprocessing import Process
+
+
+def run_tcp():
+    class RequestHandler(socketserver.BaseRequestHandler):
+
+        def handle(self) -> None:
+            self.data = self.request.recv(1024).strip()
+            print(self.data)
+            if self.data:
+                # ds = datasource.DataSource("ffreport.log")
+                self.request.sendall("hi")
+                # del ds
+
+    class Server(socketserver.TCPServer):
+        def __init__(self, server_address, RequestHandlerClass):
+            super().__init__(server_address, RequestHandlerClass)
+
+    server = Server(("127.0.0.1", 5000), RequestHandler)
+    try:
+        print("serving")
+        server.serve_forever()
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+        # def serve(self, mode):
+        #     if mode == "tcp":
+        #         print("enabling tcp mode")
+        #         with socketserver.TCPServer(self.server_address, RequestHandler) as server:
+        #             print("serving tcp")
+        #             server.handle_request()
+        #     elif mode == "udp":
+        #         print("enabling udp mode")
+        #         with socketserver.UDPServer(self.server_address, RequestHandler) as server:
+        #             print("serving udp")
+        #             server.handle_request()
+        #     else:
+        #         print("Error!")
+        #         exit(1)
 
 
 def stream(source):
@@ -14,9 +52,24 @@ if __name__ == '__main__':
     # streamProcess = Process(target=stream, args=('rtsp://rtsp.stream/pattern',))
     # streamProcess.start()
     # streamProcess.join()
-    server = server.Server()
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # serv = Server(("127.0.0.1", 5000), RequestHandler)
+    # serv.serve("tcp")
+    run_tcp()
+    # serverProcess = Process(target=server.serve, args=('tcp',))
+    # serverProcess.start()
+    # serverProcess.join()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
         sock.connect(("127.0.0.1", 5000))
         sock.sendall(bytes("getdata\n", "utf-8"))
         result = str(sock.recv(1024), "utf-8")
-    print(result)
+        print(result)
+    except IOError:
+        sys.exit(0)
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    #     print("lol")
+    #     sock.connect(("127.0.0.1", 5000))
+    #     print(sock.__str__())
+    #     sock.sendall(bytes("getdata\n", "utf-8"))
+    #     result = str(sock.recv(1024), "utf-8")
+    # print(result)
--- server.py
+++ server.py
@@ -6,21 +6,30 @@ class RequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self) -> None:
         self.data = self.request.recv(1024).strip()
+        print(self.data)
         if self.data:
-            ds = datasource.DataSource("ffreport.log")
-            self.request.sendall(ds.data)
-            del ds
+            # ds = datasource.DataSource("ffreport.log")
+            self.request.sendall("hi")
+            # del ds
 
 
 class Server:
-    def __init__(self, mode="tcp", ip="127.0.0.1", port=5000):
+    def __init__(self, ip="127.0.0.1", port=5000):
         self.ip = ip
+        print(self.ip)
         self.port = port
+        print(self.port)
+
+    def serve(self, mode):
         if mode == "tcp":
+            print("enabling tcp mode")
             with socketserver.TCPServer((self.ip, self.port), RequestHandler) as server:
+                print("serving tcp")
                 server.serve_forever()
         elif mode == "udp":
+            print("enabling udp mode")
             with socketserver.UDPServer((self.ip, self.port), RequestHandler) as server:
+                print("serving udp")
                 server.serve_forever()
         else:
             print("Error!")
