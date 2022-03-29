eb8f6b802ea691109ed3ec9384a47658dd99df49
0xB4D4SS <0xb4d4ss@gmail.com>
2022-03-29 12:00:53 +0400

    returned to socketserver package after testing

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
 import os
 import socketserver
-import socket
 import sys
 from multiprocessing import Process
 import datasource
 
 
 def run_server(datasrc):
-    HOST = ''  # Symbolic name meaning all available interfaces
-    PORT = 5000  # Arbitrary non-privileged port
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(1)
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                req = conn.recv(1024)
-                if not req:
-                    break
-                print(f"Received: {str(req)}")
-                data = datasrc.readData()
-                conn.sendall(bytes(data, "utf-8"))
 
-    # class RequestHandler(socketserver.BaseRequestHandler):
-    #
-    #     def handle(self) -> None:
-    #         data = datasrc.readData()
-    #         self.request.sendall(bytes(data, "utf-8"))
-    #
-    # class Server(socketserver.TCPServer):
-    #     def __init__(self, server_address, RequestHandlerClass):
-    #         super().__init__(server_address, RequestHandlerClass)
-    #
-    # server = Server(("127.0.0.1", 5000), RequestHandler)
-    # server.
-    # print("serving")
-    # server.serve_forever()
+    class RequestHandler(socketserver.BaseRequestHandler):
+
+        def handle(self) -> None:
+            data = datasrc.readData()
+            self.request.sendall(bytes(data, "utf-8"))
+
+    class Server(socketserver.TCPServer):
+        def __init__(self, server_address, RequestHandlerClass):
+            super().__init__(server_address, RequestHandlerClass)
+
+    server = Server(("127.0.0.1", 5000), RequestHandler)
+    print("serving")
+    server.serve_forever()
 
 
 def stream(source):
